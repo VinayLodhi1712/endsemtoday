1dac19a3b4549765931ac52265f1435d0331994e
Vinay Anand Lodhi <vinayanandlodhi12@gmail.com>
2024-04-08 00:54:09 +0530

    about and contact page complete

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,44 +1,88 @@
 import React from "react";
 import Layout from "../components/layout/layout";
 import Contactus from "../assests/Aboutus.jpg";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import products from "../assests/products.jpg"
+import stack from "../assests/stack.jpg";
+import technews from "../assests/technews.jpg";
+import data from "../assests/data.jpg";
+function Productcard() {
+  return (
+    <Card className="boxlayout" style={{ width: '19rem' }}>
+      <Card.Img variant="top" src={products} />
+      <Card.Body>
+        <Card.Title className="mediumtitlefont">Student Helper</Card.Title>
+        <Card.Text>
+          Help Students by providing them a plateform to buy and sell their study related materials
+        </Card.Text>
+        <Button variant="primary">See Products</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+function Codeconnectcard() {
+  return (
+    <Card className="boxlayout" style={{ width: '19rem' }}>
+      <Card.Img variant="top" src={stack} />
+      <Card.Body>
+        <Card.Title className="mediumtitlefont">Student Doubts Solver</Card.Title>
+        <Card.Text>
+          Help Students by providing them a plateform to ask and provide solutions of their doubts
+        </Card.Text>
+        <Button variant="primary">See Codeconnect</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+function Technewscard() {
+  return (
+    <Card className="boxlayout" style={{ width: '19rem' }}>
+      <Card.Img variant="top" src={technews} />
+      <Card.Body>
+        <Card.Title className="mediumtitlefont">Tech News Provider</Card.Title>
+        <Card.Text>
+          Help Students by providing them a plateform where they can get all current tech news
+        </Card.Text>
+        <Button variant="primary">See TechNews</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
 const About = () => {
   return (
     <Layout>
+      <p className="Titlefont center" style={{marginTop:"2rem"}}> Welcome to <mark>TALKOFCODE</mark>, your go-to destination for all
+            things code-related!</p>
+      <div className="cards">
+        <div><Productcard /></div>
+        <div className="cardmargin"><Codeconnectcard /></div>
+        <div className="cardmargin"><Technewscard /></div>
+      </div>
+
+      <p className="Titlefont center" style={{marginTop:"2rem"}}> About us</p>
       <div
         className="d-flex justify-content-center align-items-center"
         style={{ height: "100%", gap: "2rem" }}
       >
-        <div style={{ width: "50%", height: "70%" }}>
-          <img
-            src={Contactus}
+       
+        <div style={{ width: "40%", height: "75%" }}>
+          <img className="boxlayout"
+            src={data}
             style={{ width: "100%", height: "100%" }}
           ></img>
         </div>
-        <div style={{ width: "30%" }} className="flex-column d-flex ">
-          <h1
-            className="p-3 text-center"
-            style={{ backgroundColor: "black", color: "white" }}
-          >
-            {" "}
-            ABOUT US{" "}
-          </h1>
-          <p>
-            Welcome to <mark>TALKOFCODE</mark>, your go-to destination for all
-            things code-related! At TalkOfCode, we're more than just a platform
-            – we're a vibrant community of learners, creators, and enthusiasts
-            united by our passion for coding and technology. Our mission is
-            simple: to provide a dynamic space where individuals from all
-            backgrounds can come together to learn, share, and grow their coding
-            skills. Whether you're a seasoned developer looking to stay
-            up-to-date with the latest trends, a student eager to explore the
-            world of programming, or an entrepreneur seeking tech solutions,
-            TalkOfCode is here to support you every step of the way. But we're
-            not just about coding tutorials and discussions. TalkOfCode also
-            offers a unique marketplace where members can buy and sell old
-            products related to technology, helping to foster a culture of
-            sustainability and resourcefulness within our community. Let's code,
-            connect, and create together!
-          </p>
+        <div style={{ width: "50%", height:"75%" }} className="flex-column d-flex ">
+         
+        <div className="boxlayout smalltitlefont">
+    <p>At TalkOfCode, we're more than just a platform – we're a vibrant community of learners, creators, and enthusiasts united by our passion for technology. Our mission is simple: to provide a dynamic space where individuals from all backgrounds can come together to learn, share, and grow.</p>
+    <p className="smalltitlefont">"Whether you're a seasoned developer, a student, or an entrepreneur, TalkOfCode is here to support you every step of the way."</p>
+    <p className="smalltitlefont">TalkOfCode also offers a unique marketplace where members can buy and sell old tech products, fostering a culture of sustainability and resourcefulness within our community. Let's code, connect, and create together!</p>
+</div>
+
         </div>
       </div>
     </Layout>
--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,39 +1,119 @@
 import React from "react";
 import Layout from "../components/layout/layout";
-import Contactus from "../assests/contactus.png";
-import { MdEmail } from "react-icons/md";
+import Contactus from "../assests/Contactus.jpg";
+import Profile from "../assests/profile.jpg";
 import { TfiHeadphoneAlt } from "react-icons/tfi";
 import { FaPhoneAlt } from "react-icons/fa";
+import { FaGithub, FaEnvelope, FaLinkedin, FaYoutube } from 'react-icons/fa';
+import vinayprofile from "../assests/vinayprofile.jpg";
+
+const ContactForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label smalltitlefont">Name</label>
+        <input type="text" className="form-control" id="name" required placeholder="Enter your name" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label smalltitlefont">Email</label>
+        <input type="email" className="form-control" id="email" required placeholder="Enter your email" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="message" className="form-label smalltitlefont">Message</label>
+        <textarea className="form-control" id="message" rows="4" placeholder="Go ahead! We are Listening..." required></textarea>
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+  );
+};
+
 const Contact = () => {
   return (
     <Layout>
-      <div
-        className="d-flex justify-content-center align-items-center"
-        style={{ height: "100%", gap: "2rem" }}
-      >
-        <div style={{ width: "50%", height: "70%" }}>
-          <img src={Contactus} style={{ width: "100%", height: "100%" }}></img>
+      <div className="d-flex justify-content-center align-items-center formlayout">
+
+        {/* Left side: Contact Form */}
+        <div style={{ width: "50%", paddingRight: "1rem", marginRight: "2rem" }}>
+          <h1 className="Titlefont aqua">Get in Touch</h1>
+          <p className="smalltitlefont aqua">We are here for you. How can we help?</p>
+          <ContactForm />
         </div>
-        <div
-          style={{ width: "30%", fontWeight: "700" }}
-          className="flex-column d-flex "
-        >
-          <h1
-            className="p-3 text-center"
-            style={{ backgroundColor: "black", color: "white" }}
-          >
-            CONTACT US
-          </h1>
-          <p>Contact us for an kind of query</p>
-          <p>
-            <MdEmail /> : asharma7588@gmail.com
-          </p>
-          <p>
-            <FaPhoneAlt /> : +91 8817687885
-          </p>
-          <p>
-            <TfiHeadphoneAlt /> : +91 8251022728
-          </p>
+
+        {/* Right side: Contact Image and Details */}
+        <div style={{ width: "50%" }}>
+
+          <img src={Contactus} style={{ width: "80%", height: "35vh",  marginBottom: "-2rem", marginLeft:"2.5rem"}} alt="Contact Us" />
+
+          <div className="contactlayout" style={{ padding: "1rem" }}>
+            <p className="mediumtitlefont center" style={{marginTop:"0"}}>Contact us</p>
+            <div className="smalltitlefont" style={{ display: "flex", alignItems: "center", marginBottom: "1rem" }}>
+              <FaEnvelope style={{ marginRight: "0.5rem" }} /> asharma7588@gmail.com
+            </div>
+            <div className="smalltitlefont" style={{ display: "flex", alignItems: "center", marginBottom: "1rem" }}>
+              <FaPhoneAlt style={{ marginRight: "0.5rem", borderRadius: "50%" }} /> +91 8817687885
+            </div>
+            <div className="smalltitlefont" style={{ display: "flex", alignItems: "center" }}>
+              <TfiHeadphoneAlt style={{ marginRight: "0.5rem" }} /> +91 8251022728
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Team Members */}
+      <div className="Titlefont" style={{ textAlign: "center", marginTop: "2rem", marginBottom: "1rem" }}>Meet the team</div>
+      <div className="d-flex justify-content-center">
+  
+        <div className="memberinfo boxlayout">
+          <div className="memberimg">
+            <img src={Profile} className="imgstyle" alt="Person 1" />
+          </div>
+          <div style={{ marginLeft: "1rem" }}>
+            <p className="smalltitlefont" style={{color:"orange", margin:"0"}}>Ayush Sharma</p>
+            <p className="smalltitlefont" style={{margin:"0"}}>Developer and Project Manager</p>
+            <div style={{ display: "flex" }}>
+              <div className="icons" style={{ backgroundColor: "black"}}>
+                <a href="https://github.com/AyushSharma72" target="_blank" rel="noopener noreferrer" style={{ color: "white" }}><FaGithub /></a>
+              </div>
+              <div className="icons" style={{ backgroundColor: "#ff0000"}}>
+                <a href="mailto:asharma7588@gmail.com" style={{ color: "white" }}><FaEnvelope /></a>
+              </div>
+              <div className="icons" style={{ backgroundColor: "#0077b5" }}>
+                <a href="https://www.linkedin.com/in/yourlinkedin" target="_blank" rel="noopener noreferrer" style={{ color: "white" }}><FaLinkedin /></a>
+              </div>
+              <div className="icons" style={{ backgroundColor: "#8B0000"}}>
+                <a href="https://www.youtube.com/yourchannel" target="_blank" rel="noopener noreferrer" style={{ color: "white" }}><FaYoutube /></a>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+        {/* Team Member 2 */}
+        <div className="memberinfo boxlayout">
+          <div className="memberimg">
+            <img src={vinayprofile} className="imgstyle" alt="Person 1" />
+          </div>
+          <div style={{ marginLeft: "1rem" }}>
+            <p className="smalltitlefont" style={{color:"orange", margin:"0"}}>Vinay Anand Lodhi</p>
+            <p className="smalltitlefont" style={{margin:"0"}}>Developer and Project Manager</p>
+            <div style={{ display: "flex" }}>
+              <div className="icons" style={{ backgroundColor: "black"}}>
+                <a href="https://github.com/VinayLodhi1712" target="_blank" rel="noopener noreferrer" style={{ color: "white" }}><FaGithub /></a>
+              </div>
+              <div className="icons" style={{ backgroundColor: "#ff0000"}}>
+                <a href="mailto:vinayanandlodhi12@gmail.com" target="_blank" rel="noopener noreferrer" style={{ color: "white" }}><FaEnvelope /></a>
+              </div>
+              <div className="icons" style={{ backgroundColor: "#0077b5" }}>
+                <a href="https://www.linkedin.com/in/vinay-anand-lodhi-5694b1234" target="_blank" rel="noopener noreferrer" style={{ color: "white" }}><FaLinkedin /></a>
+              </div>
+              <div className="icons" style={{ backgroundColor: "#8B0000"}}>
+                <a href="https://www.youtube.com/channel/UCDSoSvpgsmz4b6gB1k3OLjg" target="_blank" rel="noopener noreferrer" style={{ color: "white" }}><FaYoutube /></a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
