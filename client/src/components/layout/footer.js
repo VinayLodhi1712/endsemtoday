1aa01dfbff0d7283ad794519bf5460f1a061f910
Vinay Anand Lodhi <vinayanandlodhi12@gmail.com>
2024-04-14 20:08:15 +0530

    footer

--- client/src/components/layout/footer.js
+++ client/src/components/layout/footer.js
@@ -1,15 +1,42 @@
 import React from "react";
 import "./../../App.css";
 import { NavLink } from "react-router-dom";
+import { FaEnvelope, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 
 function Footer() {
   return (
     <div className="bg-dark text-light p-1 footer">
-      <h3 className="text-center mt-2 pt-1">TALKOFCODE</h3>
-      <p className="text-center mt-2 ">
-        <NavLink to="/About">About</NavLink>|
-        <NavLink to="/ContactUs">Contact</NavLink>
-      </p>
+      <div className="container-fluid" style={{ marginBottom: "0.5rem" }}>
+        <div className="row" style={{ marginTop: "0.5rem" }}>
+          <div className="col-md-6 text-center">
+            <h3 className="text-center mt-2 pt-1">TALKOFCODE</h3>
+            <p>© 2024 All rights reserved.</p>
+
+          </div>
+          <div className="col-md-6 text-center">
+            <div>
+              <div style={{ marginTop: "1rem" }}>
+                <NavLink to="/About" className="mx-2">About</NavLink>|
+                <NavLink to="/ContactUs" className="mx-2">Contact</NavLink>|
+                <NavLink to="/Terms" className="mx-2">Terms</NavLink>|
+                <NavLink to="/Privacy" className="mx-2">Privacy</NavLink></div>
+
+              <div className="text-center mt-3">
+                <a href="mailto:asharma7588@gmail.com" target="_blank"><FaEnvelope className="icon" /></a>
+                <a href="https://twitter.com/VinayLodhi1712" target="_blank"><FaTwitter className="icon" /></a>
+                <a href="https://github.com/AyushSharma72" target="_blank"
+                  rel="noopener noreferrer"><FaGithub className="icon" /></a>
+                <a href="https://www.linkedin.com/in/ayush-sharma-a155a8267?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                  target="_blank"
+                  rel="noopener noreferrer"><FaLinkedin className="icon" /></a>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
     </div>
   );
 }
